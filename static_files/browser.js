a79d0890f9280fa2ac44b41c288ce7a1bab9fd5e
Jurriaan Jansen <jurriaan_@live.com>
2016-06-22 11:58:44 +0100

    Zoomed in konva layer bug fix
    Fixed a bug where some konva layers are not shown in a zoomed in
    instance. (When all anchor points are outside of the dalliance browser)

--- static_files/browser.js
+++ static_files/browser.js
@@ -211,21 +211,25 @@ function drawComparative(cache,comp,top, bottom){
 		|| (cache[i].qstart >= (bottom.viewStart | 0) && cache[i].qstart <= (bottom.viewEnd | 0))			
 		|| (cache[i].qend >= (bottom.viewStart | 0) && cache[i].qend <= (bottom.viewEnd | 0))){
 			cachesplit.push(cache[i]);
+		};		
+		//If the comparative layer is in the first position it is manually added here.
+		// if ((cache[i].rstart == 1) || cache[i].qstart == 1){
+		// 	cachesplit.push(cache[i]);
+		// };
+		if ((cache[i].rstart) <= (top.viewStart) && (cache[i].rend) >= (top.viewEnd) ||
+			(cache[i].qstart) <= (bottom.viewStart) && (cache[i].qend) >= (bottom.viewEnd)){
+			cachesplit.push(cache[i]);
 		};
 	};
-	// If user is zoomed in and the edges of the layers reach out. This function will add the single konva layer.
+	// If user is zoomed in and the edges of the layers reach out. This function will add one layer that should be visible.
 	if(cachesplit[0] == null){	
+		console.log("Debug")
 		for (var i=0; i < cache.length; i++){
 			if (((top.viewStart | 0) >=  cache[i].rstart) && ((top.viewEnd | 0) <=  cache[i].rend )){
 				cachesplit.push(cache[i]);
 			};
 		};
 	};
-
-	var group = new Konva.Group({
-		draggable: true
-	});
-
 	//Values in cachesplit will be presented onscreen.
 	cachesplit.forEach(function(m) {
 		rx1 = layerWidth * ((m.rstart - top.viewStart) / topWidth);
