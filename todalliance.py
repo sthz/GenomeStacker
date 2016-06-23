6f8f3837752e0e6602018f5471597e311c9f0cd9
Jurriaan Jansen <jurriaan_@live.com>
2016-06-23 17:20:31 +0100

    program is now multiple m8 files ready
    Previous commit I added the browser.js changes. This commits the
    todalliance changes.

--- todalliance.py
+++ todalliance.py
@@ -29,10 +29,10 @@ def main():
   
   #--Generating files for dalliance browser.
 
-  #generateDallianceFiles(genbankfiles,path)  
-  #m8toJSON(m8files,path)
+  generateDallianceFiles(genbankfiles,path)  
+  m8toJSON(m8files,path)
   generateIndex(path,genbankfiles,m8files)
-  #getStaticFiles(path)
+  getStaticFiles(path)
 
   print("----------------------")
   print("Dalliance build succesfull!")
@@ -53,12 +53,8 @@ def generateIndex(path,genbankfiles,m8files):
       if i.endswith(".catgb") or i.endswith(".gbk") or i.endswith(".gb"):
         index.write(htmlGenbank(i, genbankfilecount))
         genbankfilecount += 1
-      elif i.endswith(".m8"):
+      elif(i.endswith(".m8")):
         pass
-       # elif i.endswith(".bam"):
-       #   pass
-       # elif i.endswith(".vcf"):
-       #   pass
       else:
         print("ERROR: The file "+i+" is not yet compatible with this program.\n. todalliance ignored this file.")
   index.write(generateArrays(genbankfilecount,m8files))
@@ -83,111 +79,126 @@ def generateArrays(genbankfilecount,m8files):
   for i in range(0,genbankfilecount):
     dallianceBrowserPositions.append("[(dallianceBrowser_"+str(i+1)+".viewStart | 0), (dallianceBrowser_"+str(i+1)+".viewEnd | 0)]")
     dallianceBrowsers.append("dallianceBrowser_"+str(i+1))
-  c = 0
-  comparisons = []
-  jsonfiles = []
-  for i in m8files:
-    jsonfiles.append("'"+basename(i).replace(".m8",".json")+"'")
-    comparisons.append("\"comparison_"+str(c+1)+"_"+str(c+2)+"\"")
-    c += 1
+  comparisonArrays = createm8Array(m8files)
   return("""
-    var cache = [[]]
-    var islistening = true;"""+"\n"+
-    "var dallianceBrowsers = ["+",".join(dallianceBrowsers)+"]"+"\n"+
-    "var jsonfiles = [["+",".join(jsonfiles)+"]]"+"\n"+
-    "var comparisons = ["+",".join(comparisons)+"]"+"\n"+
-    "var dallianceBrowserPositions = ["+",".join(dallianceBrowserPositions)+"]"+"""
-    </script>
+//global
+var cache = [[]]
+var islistening = true;"""+"\n"+
+"var dallianceBrowsers = ["+",".join(dallianceBrowsers)+"]"+"\n"+
+comparisonArrays+"\n"+
+
+"var dallianceBrowserPositions = ["+",".join(dallianceBrowserPositions)+"]"+"""
+</script>
   """)
   
+
+def createm8Array(m8files):
+  comparisons = []
+  c = 0
+  m8array = []
+  tempSubArray = []
+  for i in sys.argv:
+    if i.endswith(".m8"):
+      tempSubArray.append("'"+basename(i).replace(".m8",".json")+"'")
+    else:
+      if (len(tempSubArray) != 0):
+        m8array.append("["+",".join(tempSubArray)+"]")
+        tempSubArray = []
+        comparisons.append("\"comparison_"+str(c+1)+"_"+str(c+2)+"\"")
+        c += 1
+  return("var jsonfiles = ["+",".join(m8array)+"]"+"\nvar comparisons = ["+",".join(comparisons)+"]"+"\n")
+
+
+
+
   
 def htmlGenbank(f,genbankfilecount):
   filename = basename(f).replace(".catgb","").replace(".gbk","").replace(".gb","")
   gb_record = SeqIO.read(open(f,
     "r"), "genbank")   
   html = ("""
-    var dallianceBrowser_"""+str(genbankfilecount+1)+""" = new Browser({
-        chr: '"""+gb_record.name+"""',
-        viewStart: 1,
-        viewEnd: 40000,
-        cookieKey: '"""+filename+"""',
-        pageName: 'dallianceBrowser_"""+str(genbankfilecount+1)+"""',
-        coordSystem: {
-            speciesName: '"""+gb_record.description+"""',
-            taxon: 511145,
-            auth: '',
-            version: ' '
-        },
-        disablePoweredBy: true,
-        noPersist: true,
-        toolbarBelow: false,
-        maxViewWidth: """+str(len(gb_record.seq))+""",
-        sources: [{
-            name: 'Genome',
-            twoBitURI: '"""+filename+""".2bit',
-            tier_type: 'sequence',
-            provides_entrypoints: true,
-            pinned: false
+var dallianceBrowser_"""+str(genbankfilecount+1)+""" = new Browser({
+    chr: '"""+gb_record.name+"""',
+    viewStart: 1,
+    viewEnd: 40000,
+    cookieKey: '"""+filename+"""',
+    pageName: 'dallianceBrowser_"""+str(genbankfilecount+1)+"""',
+    coordSystem: {
+        speciesName: '"""+gb_record.description+"""',
+        taxon: 511145,
+        auth: '',
+        version: ' '
+    },
+    disablePoweredBy: true,
+    noPersist: true,
+    toolbarBelow: false,
+    maxViewWidth: """+str(len(gb_record.seq))+""",
+    sources: [{
+        name: 'Genome',
+        twoBitURI: '"""+filename+""".2bit',
+        tier_type: 'sequence',
+        provides_entrypoints: true,
+        pinned: false
+    }, {
+        name: 'Genes',
+        bwgURI: '"""+filename+""".bb',
+        tier_type: 'features',
+        pinned: false,
+        provides_search: true,
+        trixURI: '"""+filename+""".ix',
+        collapseSuperGroups: true,
+        subtierMax: 1000,
+        style: [{
+            type: "default",
+            zoom: "high",
+            style: {
+                glyph: "ANCHORED_ARROW",
+                PARALLEL: true,
+                BUMP: false,
+                LABEL: false,
+                HEIGHT: 20,
+                BGITEM: true
+            }
         }, {
-            name: 'Genes',
-            bwgURI: '"""+filename+""".bb',
-            tier_type: 'features',
-            pinned: false,
-            provides_search: true,
-            trixURI: '"""+filename+""".ix',
-            collapseSuperGroups: true,
-            subtierMax: 1000,
-            style: [{
-                type: "default",
-                zoom: "high",
-                style: {
-                    glyph: "ANCHORED_ARROW",
-                    PARALLEL: true,
-                    BUMP: false,
-                    LABEL: false,
-                    HEIGHT: 20,
-                    BGITEM: true
-                }
-            }, {
-                type: "default",
-                zoom: "medium",
-                style: {
-                    glyph: "ANCHORED_ARROW",
-                    PARALLEL: true,
-                    BUMP: false,
-                    LABEL: false,
-                    HEIGHT: 20,
-                    BGITEM: true
-                }
-            }, {
-                type: "translation",
-                zoom: "low",
-                style: {
-                    glyph: "BOX",
-                    BUMP: false,
-                    LABEL: false,
-                    HEIGHT: 20,
-                    BGITEM: true
-                }
-            }]
-        }, ]
-    });
-    dallianceBrowser_"""+str(genbankfilecount+1)+""".addFeatureListener(function(event, feature, hit, tier) {
-        featureListener(event, feature, hit, tier,dallianceBrowser_"""+str(genbankfilecount+1)+""")
-    });
-    dallianceBrowser_"""+str(genbankfilecount+1)+""".addViewListener(function() {
-      if (islistening){
-        islistening = false;
-        viewListener(dallianceBrowser_"""+str(genbankfilecount+1)+""","""+str(genbankfilecount)+""")
-          islistening = true;
-      }
-    });
-    dallianceBrowser_"""+str(genbankfilecount+1)+""".addFeatureHoverListener(function(event, feature, hit, tier) {
-        mouseOver(event, feature, hit, tier);
-    });
-    dallianceBrowser_"""+str(genbankfilecount+1)+""".addFeatureNonHoverListener(mouseOut);
-    dallianceBrowser_"""+str(genbankfilecount+1)+""".disableDefaultFeaturePopup = true;
-  """)
+            type: "default",
+            zoom: "medium",
+            style: {
+                glyph: "ANCHORED_ARROW",
+                PARALLEL: true,
+                BUMP: false,
+                LABEL: false,
+                HEIGHT: 20,
+                BGITEM: true
+            }
+        }, {
+            type: "translation",
+            zoom: "low",
+            style: {
+                glyph: "BOX",
+                BUMP: false,
+                LABEL: false,
+                HEIGHT: 20,
+                BGITEM: true
+            }
+        }]
+    }, ]
+});
+dallianceBrowser_"""+str(genbankfilecount+1)+""".addFeatureListener(function(event, feature, hit, tier) {
+    featureListener(event, feature, hit, tier,dallianceBrowser_"""+str(genbankfilecount+1)+""")
+});
+dallianceBrowser_"""+str(genbankfilecount+1)+""".addViewListener(function() {
+  if (islistening){
+    islistening = false;
+    viewListener(dallianceBrowser_"""+str(genbankfilecount+1)+""","""+str(genbankfilecount)+""")
+      islistening = true;
+  }
+});
+dallianceBrowser_"""+str(genbankfilecount+1)+""".addFeatureHoverListener(function(event, feature, hit, tier) {
+    mouseOver(event, feature, hit, tier);
+});
+dallianceBrowser_"""+str(genbankfilecount+1)+""".addFeatureNonHoverListener(mouseOut);
+dallianceBrowser_"""+str(genbankfilecount+1)+""".disableDefaultFeaturePopup = true;
+""")
   if genbankfilecount != 0:
     html += "dallianceBrowser_"+str(genbankfilecount+1)+".noZoomSlider = true;"
   return (html)
@@ -196,73 +207,73 @@ def htmlGenbank(f,genbankfilecount):
 def staticIndexInfo(argument,files):
   if argument == "head":
     return("""
-      <!DOCTYPE html>
-      <!-- Jquery -->
-      <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-      <!-- Latest compiled and minified CSS -->
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-      <!-- Optional theme -->
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-      <!-- Latest compiled and minified JavaScript -->
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<!DOCTYPE html>
+<!-- Jquery -->
+<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-      <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
-      <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+<link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+<script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
 
 
-      <html>
-          <head>
-              <title>JJCT</title>
-          </head>
-        <body>
-          <div class="col-md-12 centered">
-            <hr>
-            <b> Settings </b>
-            <hr>
-            <button class="btn btn-primary" onclick="backToOverview()">Back to overview</button>
-            <button class="btn btn-primary" onclick="makeCSVG()">makeSVG</button>
-            <b>Lock browsers</b>
-            <input type="checkbox" data-toggle="toggle" id="toggleBrowserButton" checked>
-             <b>Show tooltip</b>
-            <input type="checkbox" data-toggle="toggle" id="showTooltip" checked>
-            <input type="search" id="setKonvaLayerHeight" placeholder="Set layer heigth"></input>
-            <button class="btn btn-primary" onclick="setKonvaLayerHeight()">Set</button>
-            <input type="search" id="setDesiredChangeRange" placeholder="Set desired change range"></input>
-            <button class="btn btn-primary" onclick="setDesiredChangeRange()">Set</button>
-            <button class="btn btn-primary" onclick="generateFastaFile()">display fasta file from selected sequences.</button>
-            <br>
-            <hr>
-            <b> History </b>
-             <button type="button" class="btn btn-default" aria-label="Left Align" onclick="historySave()">
-                <span class="glyphicon glyphicon glyphicon-floppy-save" aria-hidden="true"></span>
-            </button>
-            <button type="button" class="btn btn-default" aria-label="Left Align" onclick="historyBack()">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            </button>
-            <button type="button" class="btn btn-default" aria-label="Left Align" onclick="historyForward()">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            </button>
-          </div>
+<html>
+    <head>
+        <title>JJCT</title>
+    </head>
+  <body>
+    <div class="col-md-12 centered">
+      <hr>
+      <b> Settings </b>
+      <hr>
+      <button class="btn btn-primary" onclick="backToOverview()">Back to overview</button>
+      <button class="btn btn-primary" onclick="makeCSVG()">makeSVG</button>
+      <b>Lock browsers</b>
+      <input type="checkbox" data-toggle="toggle" id="toggleBrowserButton" checked>
+       <b>Show tooltip</b>
+      <input type="checkbox" data-toggle="toggle" id="showTooltip" checked>
+      <input type="search" id="setKonvaLayerHeight" placeholder="Set layer heigth"></input>
+      <button class="btn btn-primary" onclick="setKonvaLayerHeight()">Set</button>
+      <input type="search" id="setDesiredChangeRange" placeholder="Set desired change range"></input>
+      <button class="btn btn-primary" onclick="setDesiredChangeRange()">Set</button>
+      <button class="btn btn-primary" onclick="generateFastaFile()">display fasta file from selected sequences.</button>
+      <br>
+      <hr>
+      <b> History </b>
+       <button type="button" class="btn btn-default" aria-label="Left Align" onclick="historySave()">
+          <span class="glyphicon glyphicon glyphicon-floppy-save" aria-hidden="true"></span>
+      </button>
+      <button type="button" class="btn btn-default" aria-label="Left Align" onclick="historyBack()">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      </button>
+      <button type="button" class="btn btn-default" aria-label="Left Align" onclick="historyForward()">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      </button>
+    </div>
 
-      <script language="javascript" src="dalliance-all.js"></script>
-      <script language="javascript" src="konva.min.js"></script>
-      <script language="javascript" src="wz_tooltip.js"></script>
-      <script language="javascript" src="datadumper.js"></script>
-      <script language="javascript" src="browser.js"></script>
-      <script language="javascript">
-    """)
+<script language="javascript" src="dalliance-all.js"></script>
+<script language="javascript" src="konva.min.js"></script>
+<script language="javascript" src="wz_tooltip.js"></script>
+<script language="javascript" src="datadumper.js"></script>
+<script language="javascript" src="browser.js"></script>
+<script language="javascript">
+""")
   elif argument == "foot":
     return("""
-      <script language="javascript">
-        refreshBrowser()
-        loadPageFromURL()
-      </script>
+<script language="javascript">
+  refreshBrowser()
+  loadPageFromURL()
+</script>
 
-      </body>
+</body>
 
 
-      </html>
-    """)
+</html>
+""")
 
   
 def validateInputOrder():
